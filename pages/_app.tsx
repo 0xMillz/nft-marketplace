effe753bfa2dfe1c2b9e67f516207e8395f4922d
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2022-05-03 00:17:04 -0600

    Add Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Navbar from "../components/navbar"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+
+    return (
+        <>
+            <Navbar/>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
