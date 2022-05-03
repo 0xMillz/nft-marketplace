e57a42970b8ca65a4a2a1b78a7ff25c4997aeeb6
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2022-05-03 01:28:54 -0600

    Add BaseLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-import Navbar from "../components/navbar"
 
 function MyApp({Component, pageProps}: AppProps) {
 
     return (
         <>
-            <Navbar/>
             <Component {...pageProps} />
         </>
     )
