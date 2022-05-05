2c8427b72b0f2f944c446e122574a58302e9d46b
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2022-05-05 01:55:04 -0600

    Add dynamic Nftlist to marketplace

---
+++ components/nft/list/index.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent } from "react";
+import NftItem from "../item";
+
+type NftListProps = {
+  nfts: any[];
+};
+
+const NftList: FunctionComponent<NftListProps> = ({ nfts }) => {
+  return (
+    <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+      <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+        {nfts.map((nft) => (
+          <div
+            key={nft.image}
+            className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+          >
+            <NftItem item={nft} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NftList;
