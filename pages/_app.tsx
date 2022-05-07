166fd27c58fb994671e80de3fdc71c8988e35a9b
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2022-05-07 04:45:40 -0600

    Add web3 provider and truffle init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
-import '../styles/globals.css'
-import type {AppProps} from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Web3Provider } from "@providers";
 
-function MyApp({Component, pageProps}: AppProps) {
-
-    return (
-        <>
-            <Component {...pageProps} />
-        </>
-    )
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,20 @@ import type { NextPage } from "next";
 import { BaseLayout, NftList } from "@ui";
 import nfts from "../content/meta.json";
 import { NftMeta } from "@_types/nft";
+import { useWeb3 } from "@providers/web3";
 
 const Home: NextPage = () => {
+  const { provider } = useWeb3();
+
+  const getAccounts = async () => {
+    const accounts = await provider?.listAccounts();
+    console.log("accounts:", accounts);
+  };
+
+  if (provider) {
+    void getAccounts();
+  }
+
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
